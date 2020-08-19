462af701b4aeb531efcb794d3dc94e1cad22e207
Mariusz Pisz <kontakt@mariuszpisz.pl>
2020-08-19 18:44:28 +0200

    Initial project Setup

---
+++ src/test/java/pl/mariuszpisz/sampleapp/SampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mariuszpisz.sampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
